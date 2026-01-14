d9c06953817de8e3ac5200e65d0adea3dc9f5f47
digitalvottak-creator <digital.vottak@gmail.com>
2026-01-14 12:28:33 +0200

    start

---
+++ app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Подключаем папку со статикой (css, js, картинки)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Подключаем шаблоны
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def avatr(request: Request):
+    images = [
+        "/static/graphs/1346938215_14_01_2026/ads.jpeg",
+        "/static/graphs/1346938215_14_01_2026/campaigns.jpeg",
+        "/static/graphs/1346938215_14_01_2026/keywords.jpeg",
+        "/static/graphs/1346938215_14_01_2026/search_terms.jpeg"
+    ]
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "active_tab": "avatr", "images": images, "thought": "/static/thought_avatr.txt"}
+    )
+
+@app.get("/mitsubishi", response_class=HTMLResponse)
+async def mitsubishi(request: Request):
+    images = [
+        "/static/graphs/6751259696_14_01_2026/ads.jpeg",
+        "/static/graphs/6751259696_14_01_2026/campaigns.jpeg",
+        "/static/graphs/6751259696_14_01_2026/keywords.jpeg",
+        "/static/graphs/6751259696_14_01_2026/search_terms.jpeg"
+    ]
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "active_tab": "mitsubishi", "images": images, "thought": "/static/thought_mitsubishi.txt"}
+    )
